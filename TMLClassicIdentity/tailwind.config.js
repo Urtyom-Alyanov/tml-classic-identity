d2bdf2a3b2b19ed59afa2e654a15b442a6345764
Urtjom-Aljanow <mc2017artemklochkcov435@yandex.ru>
2023-02-14 20:45:38 +0300

    milestone 0.1

---
+++ TMLClassicIdentity/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'media', // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+    content: [
+        './Pages/**/*.cshtml',
+        './Views/**/*.cshtml'
+    ],
+}
